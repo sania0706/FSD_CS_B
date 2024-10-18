8cdc6e401945f2d101816159def6da67253e8c70
sania0706 <saniakhan994422@gmail.com>
2024-10-18 09:10:15 +0530

    javascript sekhenge

---
+++ javascript/distruct.js
@@ -0,0 +1,11 @@
+const student={
+    name:"xyz",
+    age:34,
+    branch :"cs",
+    section: "b"
+}
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.branch);
+console.log(student.section);
